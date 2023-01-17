c27d810f2f6edae4d322948a76b12756c2b8967a
blazhkevych.v <blazhkevych.v@gmail.com>
2023-01-17 16:45:36 +0200

    s

--- task/MainWindow.xaml.cs
+++ task/MainWindow.xaml.cs
@@ -78,7 +78,18 @@ namespace task
         public MainWindow()
         {
             InitializeComponent();
-            
+            vm = (ViewModel2048)DataContext;
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up: vm.NextStep(DirectionEnum.Up); break;
+                case Key.Down: vm.NextStep(DirectionEnum.Down); break;
+                case Key.Left: vm.NextStep(DirectionEnum.Left); break;
+                case Key.Right: vm.NextStep(DirectionEnum.Right); break;
+            }
         }
     }
 }
